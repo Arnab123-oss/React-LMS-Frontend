ebdb730c7f6900fe09f4d89ba17a0240e88105a9
Arnab123-oss <ajg616725@gmail.com>
2023-03-13 08:52:28 +0530

    redux

---
+++ src/redux/reducers/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+const server = 'https://courseboundeler.onrender.com/';
+
+const store = configureStore({
+  reducer: {},
+});
+
+export default store;
